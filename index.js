78471eec849d9bb9c96efd3176e5f26e3cb6278c
Terry-Diana <terry.wdiana@gmail.com>
2024-03-08 19:44:02 +0300

    added password and confirmPassword Javascript validation

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const wrapper = document.querySelector(".sliderWrapper");
 const menuItems = document.querySelectorAll(".menuItem");
 
+function onChange() {
+  const password = document.querySelector('input[name=password]');
+  const confirmPassword = document.querySelector('input[name=confirmPassword]');
+  if (confirmPassword.value === password.value) {
+    confirmPassword.setCustomValidity('');
+  } else {
+    confirmPassword.setCustomValidity('Passwords do not match');
+  }
+}
+
